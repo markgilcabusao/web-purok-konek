08a9a92b4d8c64fce741d51bd9c61964e53d976e
unknown <cabusaokram@gmail.com>
2025-05-04 17:02:27 +0800

    import other view pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
+import MainView from '@/views/auth/MainView.vue'
+import RequestView from '@/views/auth/RequestView.vue'
+import AppointmentView from '@/views/auth/AppointmentView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +16,21 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: MainView,
+    },
+    {
+      path: '/request',
+      name: 'request',
+      component: RequestView,
+    },
+    {
+      path: '/appointment',
+      name: 'appointment',
+      component: AppointmentView,
     },
   ],
 })
